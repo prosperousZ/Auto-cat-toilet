5931fb32e1186a2db8605fe6390f6d5302390c26
Paul <paulbrown530@gmail.com>
2020-06-16 00:36:02 -0600

    Trying to setup a chart for the cat's health. Made second page for the waste level.

--- app/src/main/java/com/example/myapplication/FirstFragment.kt
+++ app/src/main/java/com/example/myapplication/FirstFragment.kt
@@ -1,12 +1,16 @@
 package com.example.myapplication
 
+import android.R.id
+import android.R.layout
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -24,8 +28,14 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
+        view.findViewById<Button>(R.id.next_button).setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
+        view.findViewById<Button>(R.id.chart_button)
+            .setOnClickListener(View.OnClickListener {
+                val myChart =
+                    Toast.makeText(activity, "A chart would appear", Toast.LENGTH_SHORT)
+                myChart.show()
+            })
     }
 }
