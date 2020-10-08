eed9c4db41517d86830bcb83ee134a0c591d02a1
KOLBY SILIM <u1031038@umail.utah.edu>
2020-10-08 12:36:36 -0600

    Added some changes to the HealthActivity, HealthFragment, plus their respective .xml files in hopes of making the transition from the home page more fluent and easier to follow. We have not gotten the button to work on the HealthFragment but that is the plan for future sessions. Fixed the error of misplacing the button commands inside of the ListView section of the fragment_health.xml file. 8/16/2020 - Managed to get the health fragment to open the health activity class using a button to transition. We are trying to get a way to transition back to the previous page from the new activity that is not a part of the main activity. Added a versioning check to the overall file.
    10/8/2020 - Made progress on the WasteFragment.java to try and implement the activate button to control stepper motor using a webserver get request.

--- app/src/main/java/com/example/autocattoilet/ui/waste/WasteFragment.java
+++ app/src/main/java/com/example/autocattoilet/ui/waste/WasteFragment.java
@@ -1,10 +1,13 @@
 package com.example.autocattoilet.ui.waste;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,6 +16,20 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.autocattoilet.R;
+
+import org.json.JSONException;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class WasteFragment extends Fragment {
 
@@ -30,6 +47,88 @@ public class WasteFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+        Button button = (Button) root.findViewById(R.id.activate_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                new HttpRequest().execute();
+
+            }
+        });
+
         return root;
     }
+
+    public class HttpRequest extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+
+        @Override
+        protected String doInBackground(Void...voids){
+
+            byte[] result = null;
+            String text = "";
+            StringBuilder sb = new StringBuilder();
+
+            try {
+
+                URL url = new URL("http://192.168.0.23");
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+                conn.setReadTimeout(10000);
+                conn.setConnectTimeout(15000);
+                conn.setRequestMethod("POST");
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+
+                text = "\\Command=forward";
+
+                OutputStream os = conn.getOutputStream();
+                BufferedWriter  wr = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+                wr.write(url + text);
+                wr.flush();
+                wr.close();
+                os.close();
+
+                conn.connect();
+
+//                BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+//                String data;
+//
+//                while ((data = reader.readLine()) != null){
+//
+//                    sb.append(data + "\n");
+//
+//                }
+//                reader.close();
+//
+//                return sb.toString().trim();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
+    }
+
+
+
+
+
+
 }
