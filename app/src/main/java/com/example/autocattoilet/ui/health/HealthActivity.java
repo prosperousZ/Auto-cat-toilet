0570163d58097d1ab861098d4266ccb354b4355c
Paul <paulbrown530@gmail.com>
2020-10-26 22:44:59 -0600

    Testing chart implementation.

--- app/src/main/java/com/example/autocattoilet/ui/health/HealthActivity.java
+++ app/src/main/java/com/example/autocattoilet/ui/health/HealthActivity.java
@@ -1,23 +1,15 @@
 package com.example.autocattoilet.ui.health;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
-import android.app.ActionBar;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.Menu;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import com.example.autocattoilet.R;
-import com.google.android.material.navigation.NavigationView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -28,10 +20,22 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import android.graphics.Color;
+import android.os.Bundle;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
 public class HealthActivity extends AppCompatActivity {
 
     ListView listView;
     private AppBarConfiguration mAppBarConfiguration;
+    private float weightNum[];
 
    // ActionBar actionBar = getActionBar();
 
@@ -44,6 +48,15 @@ public class HealthActivity extends AppCompatActivity {
         listView = findViewById(R.id.activityLogs);
         downloadJSON("https://www.crowgotestact.com/test_service.php");
 
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        BarChart chart = (BarChart) findViewById(R.id.chart);
+//
+//        BarData data = new BarData(getXAxisValues(), getDataSet());
+//        chart.setData(data);
+//        chart.setDescription("My Chart");
+//        chart.animateXY(2000, 2000);
+//        chart.invalidate();
     }
 
     /*
@@ -98,11 +111,66 @@ public class HealthActivity extends AppCompatActivity {
     private void loadIntoListView(String json) throws JSONException {
         JSONArray jsonArray = new JSONArray(json);
         String[] databaseItems = new String[jsonArray.length()];
+        weightNum = new float[jsonArray.length()];
         for (int i = 0; i < jsonArray.length(); i++) {
             JSONObject obj = jsonArray.getJSONObject(i);
-            databaseItems[i] = obj.getString("sensor") + " | " + obj.getString("weight") + " lbs | " + obj.getString("reading_time");
+            databaseItems[i] = obj.getString("weight") + " lbs | " + obj.getString("reading_time");
+            //weightNum[i] = Float.parseFloat(obj.getString("weight"));
         }
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, databaseItems);
         listView.setAdapter(arrayAdapter);
     }
+
+//    private ArrayList getDataSet() {
+//        ArrayList dataSets = null;
+//
+//        ArrayList valueSet1 = new ArrayList();
+//        BarEntry v1e1 = new BarEntry(110.000f, 0); // Jan
+//        valueSet1.add(v1e1);
+//        BarEntry v1e2 = new BarEntry(40.000f, 1); // Feb
+//        valueSet1.add(v1e2);
+//        BarEntry v1e3 = new BarEntry(60.000f, 2); // Mar
+//        valueSet1.add(v1e3);
+//        BarEntry v1e4 = new BarEntry(30.000f, 3); // Apr
+//        valueSet1.add(v1e4);
+//        BarEntry v1e5 = new BarEntry(90.000f, 4); // May
+//        valueSet1.add(v1e5);
+//        BarEntry v1e6 = new BarEntry(100.000f, 5); // Jun
+//        valueSet1.add(v1e6);
+//
+//        ArrayList valueSet2 = new ArrayList();
+//        BarEntry v2e1 = new BarEntry(150.000f, 0); // Jan
+//        valueSet2.add(v2e1);
+//        BarEntry v2e2 = new BarEntry(90.000f, 1); // Feb
+//        valueSet2.add(v2e2);
+//        BarEntry v2e3 = new BarEntry(120.000f, 2); // Mar
+//        valueSet2.add(v2e3);
+//        BarEntry v2e4 = new BarEntry(60.000f, 3); // Apr
+//        valueSet2.add(v2e4);
+//        BarEntry v2e5 = new BarEntry(20.000f, 4); // May
+//        valueSet2.add(v2e5);
+//        BarEntry v2e6 = new BarEntry(80.000f, 5); // Jun
+//        valueSet2.add(v2e6);
+//
+//        BarDataSet barDataSet1 = new BarDataSet(valueSet1, "Brand 1");
+//        barDataSet1.setColor(Color.rgb(0, 155, 0));
+//        BarDataSet barDataSet2 = new BarDataSet(valueSet2, "Brand 2");
+//        barDataSet2.setColors(ColorTemplate.COLORFUL_COLORS);
+//
+//        dataSets = new ArrayList();
+//        dataSets.add(barDataSet1);
+//        dataSets.add(barDataSet2);
+//        return dataSets;
+//    }
+//
+//    private ArrayList getXAxisValues() {
+//        ArrayList xAxis = new ArrayList();
+//        xAxis.add("JAN");
+//        xAxis.add("FEB");
+//        xAxis.add("MAR");
+//        xAxis.add("APR");
+//        xAxis.add("MAY");
+//        xAxis.add("JUN");
+//        return xAxis;
+//    }
 }
